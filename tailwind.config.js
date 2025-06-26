29cdc1930fe274d082b2e62a9990054a9fcb7a05
abhi288282 <abhigyansengupta6@gmail.com>
2025-06-26 23:28:40 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ego: {
+          black: "#0a0a0a",
+          gray: "#1f1f1f",
+          pink: "#ff007f",
+          red: "#ff1a1a",
+          white: "#fefefe",
+          neon: "#00f0ff",
+        },
+      },
+    },
   },
   plugins: [],
 };
